c396764ea46e0b265e2252f1579972d241b2ff78
Marcel Kornblum <marcel@signal-noise.co.uk>
2018-11-06 16:33:33 +0000

    random long keey

--- appengine_config.py
+++ appengine_config.py
@@ -1,6 +1,7 @@
 from gaesessions import SessionMiddleware
 from google.appengine.ext import vendor
 import datetime
+import os
 
 # Add any libraries installed in the "lib" folder.
 vendor.add('lib')
@@ -9,7 +10,7 @@ vendor.add('lib')
 def webapp_add_wsgi_middleware(app):
     app = SessionMiddleware(
         app,
-        cookie_key="a random and long string",
+        cookie_key=os.urandom(64),
         cookie_only_threshold=0,
         lifetime=datetime.timedelta(minutes=5),
         no_datastore=True)
